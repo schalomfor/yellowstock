7e7a58df2bc3540d3fe86df0f4b2e8ac7d023c0b
schalomfor <schalomforgod@gmail.com>
2026-04-27 02:45:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+let cart = [];
+
+function addToCart(name, price) {
+  cart.push({ name, price });
+  alert(name + " ajouté au panier !");
+  updateCartCount();
+}
+
+function updateCartCount() {
+  const el = document.getElementById("cartCount");
+  if (el) el.innerText = cart.length;
+}
+
+function showCart() {
+  let total = 0;
+  let list = "";
+
+  cart.forEach(item => {
+    list += item.name + " - " + item.price + "€\n";
+    total += item.price;
+  });
+
+  alert("PANIER:\n\n" + list + "\nTOTAL: " + total + "€");
+}
+
 function buy() {
-alert("Produit ajouté au panier !");
+  alert("Simulation achat validé !");
 }
